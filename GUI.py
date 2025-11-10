634643729fc12c42edb43d6832e0a4b273670358
FemiPrograms <femistar21@gmail.com>
2025-11-10 02:20:10 +0000

    Obscure sensitive data in GUI.py
    Updated sensitive information in the code to prevent exposure.

--- GUI.py
+++ GUI.py
@@ -10,6 +10,7 @@ invoice = st.file_uploader('Upload Invoice', type="pdf")
 
 if invoice is not None:
     st.write('File Uploaded...')
-    link = 'https://olukaa.app.n8n.cloud/webhook-test/upload-invoice'
+    link = '**********'
     files = {'file' : (invoice.name, invoice.getvalue(), invoice.type or 'application/pdf' )} # I am using get value instead of seek because i dont want to reset my cursor instead just get all of the bytes at once instead of chunks 
-    resp = requests.post(link, files=files, auth=('Femi','NEA'), timeout=60)
+
+    resp = requests.post(link, files=files, auth=('****','****'), timeout=60)
